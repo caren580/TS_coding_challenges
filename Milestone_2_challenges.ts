aa5b4d5fb137f8ded56c395a7748678709cccd18
caren580 <chumacaren69@gmail.com>
2025-06-02 13:12:14 +0300

    difference between sum of even and odd numbers

--- Milestone_2_challenges.ts
+++ Milestone_2_challenges.ts
@@ -84,4 +84,19 @@ function sumEvenNumbers(arr: number[]): number {
     }
     return sum;
 }
-console.log(sumEvenNumbers([1 ,2 ,3 , 4, 5, 6]));//12
+//console.log(sumEvenNumbers([1 ,2 ,3 , 4, 5, 6]));//12
+
+//challenge 8 difference between sum of even and odd numbers
+function differenceOddEven(arr: number[]): number {
+    let sumEven = 0;
+    let sumOdd = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 === 0) {
+            sumEven += arr[i];
+        } else {
+            sumOdd += arr[i];
+        }
+    }
+    return sumEven - sumOdd;
+}
+console.log(differenceOddEven([1, 2, 3, 4, 5, 6]));//3
