015b189f68e366ceda8c72716233cd80387a74b9
caren580 <chumacaren69@gmail.com>
2025-06-02 10:18:33 +0300

    perimeter of a rectangle

--- Milestone_1_challenges.ts
+++ Milestone_1_challenges.ts
@@ -4,9 +4,15 @@ function addition(num1: number, num2: number): number{
 }
 //console.log(addition(8,5));
 
-//convert minutes into seconds
+// challenge 2 convert minutes into seconds
 function convert(minutes:number):number{
     return minutes * 60;
 }
-console.log(convert(5));//300
+//console.log(convert(5));//300
+
+//challenge 3 perimeter of a rectangle
+function findPerimeter(length:number, width:number):number{
+    return 2 * (length + width);
+}
+console.log(findPerimeter(3,2));//10
 
