7e3e9ad1f183097777559d78f66d2a91438c8bc0
caren580 <chumacaren69@gmail.com>
2025-06-02 10:14:02 +0300

    convert minutes into seconds

--- Milestone_1_challenges.ts
+++ Milestone_1_challenges.ts
@@ -2,5 +2,11 @@
 function addition(num1: number, num2: number): number{
     return num1+num2;
 }
-console.log(addition(8,5));
+//console.log(addition(8,5));
+
+//convert minutes into seconds
+function convert(minutes:number):number{
+    return minutes * 60;
+}
+console.log(convert(5));//300
 
