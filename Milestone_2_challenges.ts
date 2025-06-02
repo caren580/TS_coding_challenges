91923dafc381aa7da686081cf9b9cac7cdd2d1f7
caren580 <chumacaren69@gmail.com>
2025-06-02 12:46:43 +0300

    election winner

--- Milestone_2_challenges.ts
+++ Milestone_2_challenges.ts
@@ -20,4 +20,26 @@ function findMax(arr: number[]): number {
     }
     return max;
 }
-console.log(findMax([3,7,2,9,5]));//9
+//console.log(findMax([3,7,2,9,5]));//9
+
+//challenge 3 election winner
+type Candidate = {
+  name: string;
+  votes: number;
+};
+const candidates: Candidate[] = [
+  {name: "Alice", votes:50},
+  {name: "Bob", votes:75},
+  {name: "Charlie", votes:65}
+];
+function findWinner(arr: Candidate[]): Candidate {
+  let winner = arr[0];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].votes > winner.votes) {
+      winner = arr[i];
+    }
+  }
+  return winner;
+}
+
+console.log(findWinner(candidates)); //{name:'Bob,votes:75}
